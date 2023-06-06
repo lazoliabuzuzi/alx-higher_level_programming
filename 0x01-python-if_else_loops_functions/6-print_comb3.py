e7954f895850d727de4fda457accc547f570d3d6
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-06 16:14:25 -0700

    added space between digit and word

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for first_num in range(10):
     for second_num in range(first_num + 1, 10):
-        if first_num == 8and second_num == 9:
+        if first_num == 8 and second_num == 9:
             print("{:d}{:d}".format(first_num, second_num), end="")
         else:
             print("{:d}{:d}".format(first_num, second_num), end=", ")
