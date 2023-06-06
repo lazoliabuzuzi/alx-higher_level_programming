b04395ebc1d4c09162eed7109ee390ae081c2138
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-06 16:41:29 -0700

    changed syntax to add a character

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for first_num in range(10):
     for second_num in range(first_num + 1, 10):
         if first_num == 8 and second_num == 9:
-            print("{:d}{:d}".format(first_num, second_num), end="")
+            print("{:02d}".format(first_num * 10 + second_num), end="")
         else:
-            print("{:d}{:d}".format(first_num, second_num), end=", ")
-        if first_num < 9 and not (first_num == 8 and second_num == 9):
+            print("{:02d}".format(first_num * 10 + second_num), end=", ")
+        if first_num < 8:
             print("", end="")
