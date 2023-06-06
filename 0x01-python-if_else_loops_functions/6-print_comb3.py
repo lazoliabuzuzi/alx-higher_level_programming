26fd368686ba1e43f4c378a614d1d06c791f67e8
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-06 16:46:18 -0700

    added space after 89 for extra character

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for first_num in range(10):
     for second_num in range(first_num + 1, 10):
         if first_num == 8 and second_num == 9:
-            print("{:02d}".format(first_num * 10 + second_num), end="")
+            print("{:02d}".format(first_num * 10 + second_num), end=" ")
         else:
             print("{:02d}".format(first_num * 10 + second_num), end=", ")
         if first_num < 8:
