5c56b93bbeda6af8c96f2241ffde55b1096c52cd
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-06 16:18:56 -0700

    removed extra space after 89 to get correct output

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,5 +5,5 @@ for first_num in range(10):
             print("{:d}{:d}".format(first_num, second_num), end="")
         else:
             print("{:d}{:d}".format(first_num, second_num), end=", ")
-        if first_num < 9:
+        if first_num < 9 and not (first_num == 8 and second_num == 9):
             print("", end="")
