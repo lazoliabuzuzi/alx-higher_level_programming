d4338727b0e3634900080b56efaed5102f126ab0
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-06 16:10:47 -0700

    fixed syntax for showing two digit numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 for first_num in range(10):
     for second_num in range(first_num + 1, 10):
-        print("{:d}{:d}".format(first_num, second_num), end=", ")
-    print()
+        if first_num == 8and second_num == 9:
+            print("{:d}{:d}".format(first_num, second_num), end="")
+        else:
+            print("{:d}{:d}".format(first_num, second_num), end=", ")
+        if first_num < 9:
+            print("", end="")
