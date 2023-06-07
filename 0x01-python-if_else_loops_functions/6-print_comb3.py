d9b34060df349e42ce20fb1b660aa14d7cffeb48
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-07 14:18:42 -0700

    adjusted code so it prints a newline and removes excess zeros at beginning of numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for first_num in range(10):
     for second_num in range(first_num + 1, 10):
-        if first_num == 8 and second_num == 9:
-            print("{:02d}".format(first_num * 10 + second_num), end=" ")
+        if first_num >= second_num:
+            continue
+        elif first_num == 8 and second_num == 9:
+            print("{:d}{:d}".format(first_num, second_num))
         else:
-            print("{:02d}".format(first_num * 10 + second_num), end=", ")
-        if first_num < 8:
-            print("", end="")
+            print("{:d}{:d}, ".format(first_num, second_num), end="")
