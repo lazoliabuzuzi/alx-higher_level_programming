e79f9f5c432fd6e17587b5a39bbfbd2a2025d7ae
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-08 11:33:30 -0700

    added result to print statement and removed extra add_0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,4 +4,5 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
