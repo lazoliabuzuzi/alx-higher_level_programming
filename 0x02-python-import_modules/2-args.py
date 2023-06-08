dd1df1a851df89f8875301bfc3e0c02fda2f5e4b
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-08 12:36:47 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    num_args = len(argv) - 1
+
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+        print(f"1: {argv[1]}")
+    else:
+        print("{num_args} arguments:")
+        for a in range (num_args):
+            print(f"{a + 1} {argv[a + 1]}")
+
