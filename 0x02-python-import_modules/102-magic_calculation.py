a2de4d8c116f22c08ff4ed26c41b38d3c26d28ec
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-08 16:33:35 -0700

    extra brackets

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,8 +4,8 @@ from magic_calculation_102 import add, sub
 def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
-        for i in range(4, 7):
-        c = add(c, i)
-        return c
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
     else:
-        return sub(a, b)
+        return (sub(a, b))
