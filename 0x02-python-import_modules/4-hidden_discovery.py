418f82e20538ce547d49f2bd86b34e99ee07cfbc
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-08 14:02:25 -0700

    added if __name__ == "__main__": condition and adjusted indentation

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
+if __name__ == "__main__":
+    import hidden_4
 
-names = [name for name in dir(hidden_4) if not name.startswith('__')]
+    names = [name for name in dir(hidden_4) if not name.startswith('__')]
 
-sorted_names = sorted(names)
-for name in sorted_names:
-    print(name)
+    sorted_names = sorted(names)
+    for name in sorted_names:
+        print(name)
