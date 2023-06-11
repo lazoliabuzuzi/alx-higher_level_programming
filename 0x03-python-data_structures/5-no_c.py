bf6898833995c633796431cf3c0c8dd14001b3d2
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-11 12:14:28 -0700

    added + char to code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_string = ""
     for char in my_string:
         if char is not 'c' and char is not 'C':
-            new_string += char
+            new_string = new_string + char
     return (new_string)
