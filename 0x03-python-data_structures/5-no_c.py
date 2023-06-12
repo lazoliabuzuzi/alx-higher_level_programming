87ea5ec1e7d229fd6b5455ed488474f30a14f484
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-12 14:51:23 -0700

    changed code again

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 
 
 def no_c(my_string):
-    new_str = ""
+    new_str = ''
     for char in my_string:
-        if char.lower() != 'c':
+        if char != 'c' and char != 'C':
             new_str += char
     return new_str
