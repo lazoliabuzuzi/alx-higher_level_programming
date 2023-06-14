aaf4dddc1ab3b82b29dd5eda4e32945fe19e1724
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-14 14:05:40 -0700

    fixed nonetype object error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())
+    if a_dictionary is not None and isinstance(a_dictionary, dict):
+        sorted_keys = sorted(a_dictionary.keys())
     for key in sorted_keys:
         value = a_dictionary[key]
         print(key + ":", value)
