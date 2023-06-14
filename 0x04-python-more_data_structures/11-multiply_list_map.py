c129a4c26872c5fc93048efeac8fccb302b5bfc2
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-14 15:19:22 -0700

    reduced lines of code to three as required

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    multiplied_list = []
-
-    for num in my_list:
-        multiplied_num = num * number
-        multiplied_list.append(multiplied_num)
-
-    return multiplied_list
+    return (list(map(lambda value: value * number, my_list)))
