349eaac29942a205e1c2960a1ecd5f64c1fa6aa6
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-14 14:09:45 -0700

    fixed indentation for for statement

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,6 @@
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is not None and isinstance(a_dictionary, dict):
         sorted_keys = sorted(a_dictionary.keys())
-    for key in sorted_keys:
-        value = a_dictionary[key]
-        print(key + ":", value)
+        for key in sorted_keys:
+            value = a_dictionary[key]
+            print(key + ":", value)
