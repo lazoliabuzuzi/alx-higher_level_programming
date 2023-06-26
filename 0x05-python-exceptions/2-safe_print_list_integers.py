0bf847658f16ca8e4ea8dede44dde8fe72fe2126
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-26 14:38:00 -0700

    fixed indentation and removed finally

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
-
-    try:
-        for a in range(x):
+    for a in range(x):
+        try:
             if isinstance(my_list[a], int):
                 print("{:d}".format(my_list[a]), end="")
                 counter += 1
-    except (ValueError, TypeError):
-        pass
-    finally:
-        print()
-
+        except (ValueError, TypeError):
+            pass
+    print("")
     return counter
