8ba7fea4393389939a859e7afff1a3ee2fa3201f
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-06-26 07:00:06 -0700

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+
+    try:
+        for a in range(x):
+            if isinstance(my_list[a], int):
+                print("{:d}".format(my_list[a]), end="")
+                counter += 1
+    except (IndexError, TypeError):
+        pass
+    finally:
+        print()
+
+    return counter
