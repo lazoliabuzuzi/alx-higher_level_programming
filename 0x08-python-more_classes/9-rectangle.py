fb0fb8866ab71c407ff2d22cf010423ef700d73a
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-07-03 17:24:09 -0700

    added code to staticmethod

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -80,7 +80,11 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise ValueError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
+
+        area_1 = rect_1.area()
+        area_2 = rect_2.area()
+
+        if area_1 >= area_2:
             return rect_1
         else:
             return rect_2
