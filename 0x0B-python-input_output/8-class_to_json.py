03763a57826cffda6a4600093b5b81ca7de548b4
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-07-11 12:32:37 -0700

    changed the code

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
 """Define a dictionary description."""
-import sys
 
 
 def class_to_json(obj):
-    """Return a dictionary description with simple data structure.
-
-    Args:
-        object: An instance of a class.
-
-    Returns:
-        dict: The dictionary description with a simple data structure.
-    """
-    attributes = vars(obj)
-    serialized = {}
-
-    for attr, value in attributes.items():
-        if isinstance(value, (list, dict, str, int, bool)):
-            serialized[attr] = value
-
-    return serialized
+    """Return a dictionary description with simple data structure."""
+    return obj.__dict__
