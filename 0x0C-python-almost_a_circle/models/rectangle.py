fb25685f08d359c50580c3a392149c0f1e782822
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-07-16 11:48:42 -0700

    added public method area

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Return the area of the rectangle instance."""
+        return self.__width * self.__height
