2a53d2179c6bdd836b7538d0e2ee343c55682999
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-07-21 13:40:08 -0700

    fixed property setter of x

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -65,7 +65,7 @@ class Rectangle(Base):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must >= 0")
-        self.__y = value
+        self.__x = value
 
     @property
     def y(self):
@@ -96,3 +96,16 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
+
+    def update(self, *args):
+        """Update the attributes of the rectangle."""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
