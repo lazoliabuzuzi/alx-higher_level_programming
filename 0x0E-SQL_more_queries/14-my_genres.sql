98b025740f30dd8b48913c08828e3c8bc5927091
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-08-17 14:10:40 -0700

    changed naming syntax

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,10 +4,9 @@
 -- Results must be sorted in ascending order by the genre name
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT name
+SELECT tv_genres.name
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
-GROUP BY name
-ORDER BY name ASC;
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name;
